ebeb3d5c8ef8fc9cc2e91ba60ee90905fb32eb7f
AceReX <ace.rexnoir@gmail.com>
2024-04-23 09:29:50 +0530

    Make a pair of assets for about page

--- src/about.js
+++ src/about.js
@@ -1,5 +1,15 @@
-import "./style.css";
+import "./about.css";
+import { createElement } from "./index";
+
 export default function about() {
+  //reset the container
   let container = document.querySelector("#content");
-  container.textContent = "Love you:";
+  container.innerHTML = "";
+
+  let about_container = createElement("div", "about-container");
+  about_container.textContent = "HEll";
+  let span = createElement("span", "span");
+  span.textContent = "HELLLOO";
+  about_container.appendChild(span);
+  container.appendChild(span);
 }
